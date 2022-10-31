55fada6827037528a3b493e345bee98b31a92a86
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-31 12:18:55 +0530

    initial commit

---
+++ Solidity Path: Beginner to Intermediate Smart Contracts/Zombie Battle System/ZombieHelper.sol
@@ -0,0 +1,66 @@
+/*
+payable Modifier:-
+    payable functions are part of what makes Solidity and Ethereum so cool — they 
+are a special type of function that can receive Ether.
+
+contract OnlineStore {
+  function buySomething() external payable {
+    // Check to make sure 0.001 ether was sent to the function call:
+    require(msg.value == 0.001 ether);
+    // If so, some logic to transfer the digital item to the caller of the function:
+    transferThing(msg.sender);
+  }
+}
+*/
+
+/*
+// Assuming `OnlineStore` points to your contract on Ethereum:
+OnlineStore.buySomething({from: web3.eth.defaultAccount, value: web3.utils.toWei(0.001)})
+*/
+
+
+
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./Zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  // 1. Define levelUpFee here
+  uint levelUpFee=0.001 ether;
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
+  // 2. Insert levelUp function here
+  function levelUp(uint _zombieId) external payable{
+    require(msg.value==levelUpFee);
+    zombies[_zombieId].level++;
+  }
+
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+  }
+
+}
