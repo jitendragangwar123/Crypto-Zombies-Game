d29d50f3c353a0e9d4eab1b7783fbce5cdaa499d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-31 19:15:34 +0530

    initial commit

--- Solidity Path: Beginner to Intermediate Smart Contracts/5_ERC721 & Crypto-Collectibles/ZombieFeeding.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/5_ERC721 & Crypto-Collectibles/ZombieFeeding.sol
@@ -1,7 +1,5 @@
 pragma solidity >=0.5.0 <0.6.0;
-
-import "./Zombiefactory.sol";
-
+import "./zombiefactory.sol";
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -21,7 +19,8 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  modifier ownerOf(uint _zombieId) {
+  // 1. Change modifier name to `onlyOwnerOf`
+  modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
@@ -38,7 +37,8 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+  // 2. Change modifier name here as well
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
