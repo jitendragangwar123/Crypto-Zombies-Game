309b40dee5a6fbdd200332221acba510a101e689
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-31 14:45:02 +0530

    initial commit

--- Solidity Path: Beginner to Intermediate Smart Contracts/Zombie Battle System/ZombieHelper.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/Zombie Battle System/ZombieHelper.sol
@@ -18,26 +18,53 @@ contract OnlineStore {
 OnlineStore.buySomething({from: web3.eth.defaultAccount, value: web3.utils.toWei(0.001)})
 */
 
+/* withdraw:- 
+        After you send Ether to a contract, it gets stored in the contract's Ethereum account, 
+and it will be trapped there — unless you add a function to withdraw the Ether from the contract.
+
+contract GetPaid is Ownable {
+  function withdraw() external onlyOwner {
+    address payable _owner = address(uint160(owner()));
+    _owner.transfer(address(this).balance);
+  }
+}
+
+:-We're using owner() and onlyOwner from the Ownable contract, assuming that was imported.
+
+:-It is important to note that you cannot transfer Ether to an address unless that address is of type address payable.
+But the _owner variable is of type uint160, meaning that we must explicitly cast it to address payable.
+:-Once you cast the address from uint160 to address payable, you can transfer Ether to that address using the transfer function, 
+and address(this).balance will return the total balance stored on the contract.
+*/
+
 
 
 
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./Zombiefeeding.sol";
+import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // 1. Define levelUpFee here
-  uint levelUpFee=0.001 ether;
+  uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
 
-  // 2. Insert levelUp function here
-  function levelUp(uint _zombieId) external payable{
-    require(msg.value==levelUpFee);
+  // 1. Create withdraw function here
+  function withdraw() external onlyOwner{
+    address payable _owner=address(uint160(owner()));
+    _owner.transfer(address(this).balance);
+  }
+  // 2. Create setLevelUpFee function here
+  function setLevelUpFee(uint _fee) external onlyOwner{
+    levelUpFee=_fee;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
     zombies[_zombieId].level++;
   }
 
