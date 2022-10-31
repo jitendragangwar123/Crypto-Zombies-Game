8b5bbdf82e67e7aaa66afc11758ff739efc1b7c1
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-31 17:27:12 +0530

    initial commit

--- Solidity Path: Beginner to Intermediate Smart Contracts/4_Zombie Battle System/ZombieAttack.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/4_Zombie Battle System/ZombieAttack.sol
@@ -29,14 +29,23 @@ contract ZombieAttack is ZombieHelper {
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
-
   }
 
-  // 1. Add modifier here
   function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
-    // 2. Start function definition here
-    Zombie storage myZombie=zombies[_zombieId];
-    Zombie storage enemyZombie=zombies[_targetId];
-    uint rand=randMod(100);
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } // start here
+    else{
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
   }
 }
+
