c0888c4316fc620025137919ccea162cfd77a31d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-31 16:21:56 +0530

    initial commit

--- Solidity Path: Beginner to Intermediate Smart Contracts/4_Zombie Battle System/ZombieAttack.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/4_Zombie Battle System/ZombieAttack.sol
@@ -20,20 +20,23 @@ and move on to trying to solve the next block.
 
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombiehelper.sol";
+import "./Zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
   uint randNonce = 0;
-  // Create attackVictoryProbability here
-  uint attackVictoryProbability=70;
+  uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+
   }
 
-  // Create new function here
-  function attack(uint _zombieId,uint _targetId)external{
-    
+  // 1. Add modifier here
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    // 2. Start function definition here
+    Zombie storage myZombie=zombies[_zombieId];
+    Zombie storage enemyZombie=zombies[_targetId];
+    uint rand=randMod(100);
   }
 }
